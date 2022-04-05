c06197f4fe7946e9b79926ffd49f515dc0293552
squiggs <brennan@bell.net>
2022-04-05 19:06:05 -0400

    first test

---
+++ pages/earth/country/index.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+const Cont = () => {
+    return ( 
+
+        
+        <div>
+    <h1>which country you where born on</h1>
+    <Link href="earth/country/canada"><a>pages/earth/country Canada</a></Link> <br />
+    <Link href="/"><a>back to home</a></Link> <br />
+    
+    
+   </div>
+     );
+}
+ 
+export default Cont;
---
+++ pages/earth/index.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+const Base = () => {
+    return ( 
+
+        
+        <div>
+    <h1>which continent you where born on</h1>
+    <Link href="earth/southamerica"><a>South America</a></Link> <br />
+    <Link href="earth/northamerica"><a>North America</a></Link> <br />
+    
+    <Link href="/"><a>back to home page</a></Link> <br />
+   
+
+    
+   </div>
+     );
+}
+ 
+export default Base;
